302549b4876d97f96231d6e26057f9f967aefad5
YlStar1977 <128179443+YlStar1977@users.noreply.github.com>
2023-07-31 13:51:15 +0300

    push 2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,63 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+import { galleryItems } from './gallery-items.js';
+
+const galleryList = document.querySelector('.gallery');
+
+function createGalleryItemsMarkup(items) {
+    return items.map(({ preview, original, description }) => {
+        return `
+            <li class="gallery__item">
+                <a class="gallery__link" href="${original}">
+                    <img
+                        class="gallery__image"
+                        src="${preview}"
+                        data-source="${original}"
+                        alt="${description}"
+                    />
+                </a>
+            </li>
+        `;
+    }).join('');
+}
+
+
+galleryList.insertAdjacentHTML('beforeend', createGalleryItemsMarkup(galleryItems));
+
+galleryList.addEventListener('click', onGalleryItemClick);
+
+
+function onGalleryItemClick(event) {
+    event.preventDefault();
+    
+
+    if (event.target.nodeName !== 'IMG') {
+        return;
+    }
+
+ 
+    const largeImageUrl = event.target.dataset.source;
+
+  
+    openModal(largeImageUrl);
+}
+
+function openModal(url) {
+    const instance =  new SimpleLightbox(`
+        <img src="${url}" alt="Image description" />
+    `);
+
+    instance.show();
+
+    document.addEventListener('keydown', onModalKeyPress);
+
+  
+    function onModalKeyPress(event) {
+        if (event.key === 'Escape') {
+            instance.close();
+            document.removeEventListener('keydown', onModalKeyPress);
+        }
+    }
+}
 
 console.log(galleryItems);
