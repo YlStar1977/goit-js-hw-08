4508f5a87ee8f5843d53c75c50233e0dff8d0b9e
YlStar1977 <128179443+YlStar1977@users.noreply.github.com>
2023-07-31 14:55:05 +0300

    task 02 03

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,33 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const player = new Player('vimeo-player');
+const videoLocalStorageKey = 'videoplayer-current-time';
+
+
+function saveCurrentTime(time) {
+  localStorage.setItem(videoLocalStorageKey, time);
+}
+
+
+function getStoredTime() {
+  const storedTime = localStorage.getItem(videoLocalStorageKey);
+  return storedTime ? parseFloat(storedTime) : 0;
+}
+
+
+const throttledSaveCurrentTime = throttle(saveCurrentTime, 1000);
+
+
+player.ready().then(() => {
+
+  const initialTime = getStoredTime();
+
+  player.setCurrentTime(initialTime);
+
+
+  player.on('timeupdate', (data) => {
+    const currentTime = data.seconds;
+    throttledSaveCurrentTime(currentTime);
+  });
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,52 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const emailInput = form.querySelector('input[name="email"]');
+const messageInput = form.querySelector('textarea[name="message"]');
+const feedbackLocalStorageKey = 'feedback-form-state';
+
+
+function getStoredFormState() {
+  const storedState = localStorage.getItem(feedbackLocalStorageKey);
+  return storedState ? JSON.parse(storedState) : {};
+}
+
+
+function saveFormState() {
+  const formState = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+  localStorage.setItem(feedbackLocalStorageKey, JSON.stringify(formState));
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+  const storedFormState = getStoredFormState();
+  emailInput.value = storedFormState.email || '';
+  messageInput.value = storedFormState.message || '';
+});
+
+
+const throttledSaveFormState = throttle(saveFormState, 500);
+
+form.addEventListener('input', () => {
+  throttledSaveFormState();
+});
+
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const formState = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+
+ 
+  emailInput.value = '';
+  messageInput.value = '';
+
+  localStorage.removeItem(feedbackLocalStorageKey);
+
+
+  console.log(formState);
+});
