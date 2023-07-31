a4803e562ae082fc214e8cbe6cf77569bd77c4fb
YlStar1977 <128179443+YlStar1977@users.noreply.github.com>
2023-07-31 17:18:06 +0300

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -23,41 +23,10 @@ function createGalleryItemsMarkup(items) {
 
 
 galleryList.insertAdjacentHTML('beforeend', createGalleryItemsMarkup(galleryItems));
-
-galleryList.addEventListener('click', onGalleryItemClick);
-
-
-function onGalleryItemClick(event) {
-    event.preventDefault();
+ const lightbox = new SimpleLightbox('.gallery a', {
+    captions: 'true',
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,
     
-
-    if (event.target.nodeName !== 'IMG') {
-        return;
-    }
-
- 
-    const largeImageUrl = event.target.dataset.source;
-
-  
-    // openModal(largeImageUrl);
-}
-
-function openModal(url) {
-    const instance =  new SimpleLightbox(`
-        <img src="${url}" alt="Image description" />
-    `);
-
-    instance.show();
-
-    document.addEventListener('keydown', onModalKeyPress);
-
-  
-    function onModalKeyPress(event) {
-        if (event.key === 'Escape') {
-            instance.close();
-            document.removeEventListener('keydown', onModalKeyPress);
-        }
-    }
-}
-
-console.log(galleryItems);
+  });
